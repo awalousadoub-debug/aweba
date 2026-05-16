861c48c1610a6af436fdf7c8ff6ccc0cca96d361
AWALOU SADOU <awalousadoub@gmail.com>
2026-05-16 15:04:00 +0100

    fix api url production

--- logisaas-frontend/src/api/index.js
+++ logisaas-frontend/src/api/index.js
@@ -1,8 +1,6 @@
 import axios from 'axios';
 
-const api = axios.create({
-  baseURL: 'https://aweba-1.onrender.com/api'
-});
+const api = axios.create({ baseURL: 'https://aweba-1.onrender.com/api' });
 
 api.interceptors.request.use(config => {
   const token = localStorage.getItem('token');
