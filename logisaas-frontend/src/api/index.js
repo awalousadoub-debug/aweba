5128b4dd28a48a73d977fb0d8221a08a0e28b4ad
AWALOU SADOU <awalousadoub@gmail.com>
2026-05-19 08:40:33 +0100

    fix cors et api url

--- logisaas-frontend/src/api/index.js
+++ logisaas-frontend/src/api/index.js
@@ -1,6 +1,10 @@
 import axios from 'axios';
 
-const api = axios.create({ baseURL: 'https://aweba-1.onrender.com/api' });
+const api = axios.create({
+  baseURL: process.env.REACT_APP_API_URL
+    ? process.env.REACT_APP_API_URL + '/api'
+    : 'http://localhost:4000/api'
+});
 
 api.interceptors.request.use(config => {
   const token = localStorage.getItem('token');
